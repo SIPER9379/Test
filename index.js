d922437c8d2afbe83a53dbe1e0b8d168cb5509a6
SIPER9379 <dhruq@naver.com>
2024-11-18 21:57:19 +0900

    initial

---
+++ index.js
@@ -0,0 +1,22 @@
+// let figlet = require("figlet");
+
+// figlet("Hello World!!", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+
+// const express = require('express')
+// const app = express()
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+// app.listen(3000)
+
+setTimeout(()=>{console.log("1초 지남")}, 1000);
